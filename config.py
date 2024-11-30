45fa3d6ae8befb5b3d166956374c6ed1abac3c03
Kimdabin24 <gimd1580@gmail.com>
2024-12-01 03:02:44 +0900

    데이터베이스 테스트

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
+from flask import Flask
+from dotenv import load_dotenv
 import os
+
+# .env 파일을 로드하여 환경 변수 설정
+load_dotenv()
 
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'yourpassword')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'library_management')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '1234')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'ebookstore')
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+if __name__ == '__main__':
+    app.run(debug=True)
