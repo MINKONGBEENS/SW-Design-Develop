93883fb992b013a9800e7fc4ebc449f97f150179
Kimdabin24 <gimd1580@gmail.com>
2024-12-02 14:24:56 +0900

    사이트 연결중

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -16,27 +16,45 @@ def change_banner(direction):
     return banners[current_index]
 
 @app.route('/')
-def index():
+def home():
     global current_index
-    # 현재 배너 이미지 반환
+    banners = ['image_2.png', 'image_3.png', 'image_4.png']
+
     current_banner = banners[current_index]
-    # 이전, 현재, 다음 배너의 이미지 반환
     prev_banner = banners[(current_index - 1) % len(banners)]
     next_banner = banners[(current_index + 1) % len(banners)]
+
     return render_template('index.html', current_banner=current_banner, prev_banner=prev_banner, next_banner=next_banner)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-# app.py
-from flask import Flask
-from config import Config
-from dotenv import load_dotenv
+# 로그인 페이지 라우트
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-# .env 파일을 로드
-load_dotenv()
+# 회원가입 페이지 라우트
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-app = Flask(__name__)
-app.config.from_object(Config)
+# 자유게시판 페이지 라우트
+@app.route('/community-board')
+def community_board():
+    return render_template('community-board.html')
+
+# 자료실 페이지 라우트
+@app.route('/datalist-board')
+def datalist_board():
+    return render_template('datalist-board.html')
+
+# 이벤트 페이지 라우트
+@app.route('/event-board')
+def event_board():
+    return render_template('event-board.html')
+
+# 공지사항 페이지 라우트
+@app.route('/notice-board')
+def notice_board():
+    return render_template('notice-board.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
